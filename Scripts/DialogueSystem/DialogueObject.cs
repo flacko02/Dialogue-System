54a5a86a8fea478fe9ec1115938d42f875c556d2
flacko02 <92368108+flacko02@users.noreply.github.com>
2021-12-05 20:10:47 +0300

    Add files via upload

---
+++ Scripts/DialogueSystem/DialogueObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/DialogueObject")] // automatiski Unity, kad vēlies sataisīt dialogu objektu, uz labas peles pogas vinam atveras opcija
+public class DialogueObject : ScriptableObject
+{
+    [SerializeField] [TextArea] private string[] dialogue; // iekšā Unity var pievienot vairākus dialogus, un izveleties tos
+    [SerializeField] private Response[] responses; // Atbildes dialogam
+
+    public string[] Dialogue => dialogue;
+
+    public bool HasResponses => Responses != null && Responses.Length > 0;
+
+    public Response[] Responses => responses;
+}
